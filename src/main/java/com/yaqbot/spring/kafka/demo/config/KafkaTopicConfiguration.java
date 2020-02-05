3a3f93f6bbf9e346a40bc492c25d118294d0c757
Yvette Quinby <yvette.quinby@protonmail.com>
2020-02-05 17:04:50 +0100

    error handling

--- src/main/java/com/yaqbot/spring/kafka/demo/config/KafkaTopicConfiguration.java
+++ src/main/java/com/yaqbot/spring/kafka/demo/config/KafkaTopicConfiguration.java
@@ -1,17 +1,34 @@
 package com.yaqbot.spring.kafka.demo.config;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
 
 @Configuration
+@RequiredArgsConstructor
 public class KafkaTopicConfiguration {
 
   public static final String TOPIC_DEMO = "demo-topic";
 
+  private final ConsumerFactory<Object, Object> kafkaConsumerFactory;
+  private final ConcurrentKafkaListenerContainerFactoryConfigurer kafkaListenerContainerFactoryConfigurer;
+
   @Bean
   public NewTopic demoTopic() {
     return new NewTopic(TOPIC_DEMO, 1, (short) 1);
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    kafkaListenerContainerFactoryConfigurer.configure(factory, kafkaConsumerFactory);
+    factory.setErrorHandler(new SeekToCurrentErrorHandler()); // will retry 10 times
+    return factory;
   }
 
 
--- src/main/java/com/yaqbot/spring/kafka/demo/listener/DemoListener.java
+++ src/main/java/com/yaqbot/spring/kafka/demo/listener/DemoListener.java
@@ -12,11 +12,14 @@ import javax.transaction.Transactional;
 @Slf4j
 public class DemoListener {
 
-  @Transactional
+  private static final String FORCE_ERROR_TITLE = "error";
+
   @KafkaListener(topics = KafkaTopicConfiguration.TOPIC_DEMO)
   public void listen(DemoDto payload) {
+    if(FORCE_ERROR_TITLE.equals(payload.getTitle())) {
+      throw new RuntimeException("Forcing exception to demo error handling.");
+    }
     log.info("\n--- RECEIVED MESSAGE FROM KAFKA ---\n" + payload + "\n--- --- ---");
   }
-
 
 }
